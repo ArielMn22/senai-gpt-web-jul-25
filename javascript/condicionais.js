1a6fc6053e4583724a0ba46c3244d9e5234ad407
Ariel Paixão <arielpaixao10@gmail.com>
2025-08-22 21:50:34 -0300

    Adiciona novos arquivos JavaScript e HTML, e remove espaços desnecessários no CSS

---
+++ javascript/condicionais.js
@@ -0,0 +1,12 @@
+// Exercício 1
+let idade = prompt("Qual é a idade?");
+
+if (idade >= 18) {
+
+    alert("Parabéns! Você é maior de idade.");
+
+} else {
+
+    alert("Vai pra Super Nani!");
+
+}
---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+// console.log("Hello World!");
+
+// const valorFixo = "Ariel"; // Valor Fixo / Hardcoded
+
+// let nome = prompt("Qual seu nome?"); // Valor dinâmico
+// let idade = prompt("Qual a sua idade?"); // Valor dinâmico
+
+// alert("Meu nome é " + nome);
+
+// let teste = "Azul";
+// console.log("Teste: ", teste);
+// teste = "Laranja";
+// console.log("Teste: ", teste);
+
+// var teste = "Azul";
+// var teste = "Laranja";
+// console.log(teste);
+
+// const valorConstante = prompt("Insira algo:");
