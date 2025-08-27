d896c44273af37df59ad14227325e647756f2aa7
Ariel Paixão <arielpaixao10@gmail.com>
2025-08-27 19:16:18 -0300

    Comenta exercícios e adiciona lógica para calculadora

--- javascript/condicionais.js
+++ javascript/condicionais.js
@@ -1,12 +1,67 @@
 // Exercício 1
-let idade = prompt("Qual é a idade?");
+// let idade = prompt("Qual sua idade?");
 
-if (idade >= 18) {
+// if (idade >= 18) {
 
-    alert("Parabéns! Você é maior de idade.");
+//     alert("Maior de idade!");
 
+// } else {
+//     // Bloco do falso
+//     alert("Vá pra super-nani!");
+// }
+
+// Exercício 2
+// ...
+
+// Exercício 5
+// let idade = prompt("5. Insira sua idade:");
+
+// if (idade > 0 && idade <= 12) {
+
+//     alert("Criança.");
+
+// } else {
+
+//     if (idade > 12 && idade <= 17) {
+
+//         alert("Adolescente.");
+
+//     }
+
+// }
+
+// Exercício 6
+// let num = prompt("Insira um número: ");
+
+// if (num % 2 == 0) {
+
+//     alert("Par");
+
+// } else {
+
+//     alert("Impar");
+
+// }
+
+// Intermediário 1 - Calculadora
+let numero1 = Number(prompt("Insira o primeiro número:"));
+console.log(numero1);
+
+let numero2 = Number(prompt("Insira o segundo número:"));
+let operacao = prompt("Insira a operação. Exemplo: +, -, * ou /");
+
+let resultado = 0;
+
+if (operacao == "+") {
+  resultado = numero1 + numero2;
+} else if (operacao == "-") {
+  resultado = numero1 - numero2;
+} else if (operacao == "*") {
+  resultado = numero1 * numero2;
+} else if (operacao == "/") {
+  resultado = numero1 / numero2;
 } else {
-
-    alert("Vai pra Super Nani!");
-
+  alert("Opção inválida! Leia a pergunta.");
 }
+
+alert(resultado);
