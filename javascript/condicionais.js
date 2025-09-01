1bb0046632eebd552739fd7d2418833c1013239d
Ariel Paixão <arielpaixao10@gmail.com>
2025-09-01 18:54:09 -0300

    feat(javascript): central de exercícios.

--- javascript/condicionais.js
+++ javascript/condicionais.js
@@ -1,34 +1,35 @@
 // Exercício 1
-// let idade = prompt("Qual sua idade?");
+function exercicio01() {
+  let idade = prompt("Qual sua idade?");
 
-// if (idade >= 18) {
+  if (idade >= 18) {
+    alert("Maior de idade!");
+  } else {
+    // Bloco do falso
+    alert("Vá pra super-nani!");
+  }
 
-//     alert("Maior de idade!");
-
-// } else {
-//     // Bloco do falso
-//     alert("Vá pra super-nani!");
-// }
+  adicionarLog();
+}
 
 // Exercício 2
 // ...
 
 // Exercício 5
-// let idade = prompt("5. Insira sua idade:");
 
-// if (idade > 0 && idade <= 12) {
+function exercicio05() {
 
-//     alert("Criança.");
+  let idade = prompt("5. Insira sua idade:");
 
-// } else {
+  if (idade > 0 && idade <= 12) {
+    alert("Criança.");
+  } else {
+    if (idade > 12 && idade <= 17) {
+      alert("Adolescente.");
+    }
+  }
 
-//     if (idade > 12 && idade <= 17) {
-
-//         alert("Adolescente.");
-
-//     }
-
-// }
+}
 
 // Exercício 6
 // let num = prompt("Insira um número: ");
@@ -44,24 +45,24 @@
 // }
 
 // Intermediário 1 - Calculadora
-let numero1 = Number(prompt("Insira o primeiro número:"));
-console.log(numero1);
+// let numero1 = Number(prompt("Insira o primeiro número:"));
+// console.log(numero1);
 
-let numero2 = Number(prompt("Insira o segundo número:"));
-let operacao = prompt("Insira a operação. Exemplo: +, -, * ou /");
+// let numero2 = Number(prompt("Insira o segundo número:"));
+// let operacao = prompt("Insira a operação. Exemplo: +, -, * ou /");
 
-let resultado = 0;
+// let resultado = 0;
 
-if (operacao == "+") {
-  resultado = numero1 + numero2;
-} else if (operacao == "-") {
-  resultado = numero1 - numero2;
-} else if (operacao == "*") {
-  resultado = numero1 * numero2;
-} else if (operacao == "/") {
-  resultado = numero1 / numero2;
-} else {
-  alert("Opção inválida! Leia a pergunta.");
-}
+// if (operacao == "+") {
+//   resultado = numero1 + numero2;
+// } else if (operacao == "-") {
+//   resultado = numero1 - numero2;
+// } else if (operacao == "*") {
+//   resultado = numero1 * numero2;
+// } else if (operacao == "/") {
+//   resultado = numero1 / numero2;
+// } else {
+//   alert("Opção inválida! Leia a pergunta.");
+// }
 
-alert(resultado);
+// alert(resultado);
--- javascript/repeticoes.js
+++ javascript/repeticoes.js
@@ -1,47 +1,65 @@
+function adicionarLog () {
+
+  let divLogs = document.querySelector(".logs"); // Seleciona a div.
+
+  let tagP = document.createElement("p"); // Cria a tag p.
+
+  tagP.innerHTML = "Exercício executado em " + new Date().toLocaleDateString() + " às " + new Date().toLocaleTimeString();
+
+  divLogs.appendChild(tagP); // Acrescenta a tagP dentro da divLogs.
+
+}
+
 console.log("FOR");
-for (let contadora = 1; contadora <= 5; contadora++) {
+function rep01 () {
 
-  console.log(contadora);
+  for (let contadora = 1; contadora <= 5; contadora++) {
 
-}
-
-console.log("WHILE");
-let contadoraWhile = 1;
-while (contadoraWhile <= 5) {
-
-  console.log(contadoraWhile);
-
-  contadoraWhile++;
-
-}
-
-console.log("DO WHILE SIMPLES");
-
-do {
-
-  console.log("Mostrou o valor na tela, ainda que a condição seja falsa.")
-
-} while (1 == 2);
-
-console.log("DO WHILE COMPLEXO");
-
-let operacao = "";
-do {
-
-  operacao = prompt("Qual operação deseja fazer?");
-
-  if (operacao != "+" && 
-      operacao != "-" && 
-      operacao != "*" && 
-      operacao != "/") {
-
-    alert("Operação inválida.");
+    console.log(contadora);
 
   }
 
-} while (operacao != "+" && 
-        operacao != "-" && 
-        operacao != "*" && 
-        operacao != "/");
+  alert("Exercício executado no console.");
 
-alert("Opção válida!");
+}
+
+// console.log("WHILE");
+// let contadoraWhile = 1;
+// while (contadoraWhile <= 5) {
+
+//   console.log(contadoraWhile);
+
+//   contadoraWhile++;
+
+// }
+
+// console.log("DO WHILE SIMPLES");
+
+// do {
+
+//   console.log("Mostrou o valor na tela, ainda que a condição seja falsa.")
+
+// } while (1 == 2);
+
+// console.log("DO WHILE COMPLEXO");
+
+// let operacao = "";
+// do {
+
+//   operacao = prompt("Qual operação deseja fazer?");
+
+//   if (operacao != "+" && 
+//       operacao != "-" && 
+//       operacao != "*" && 
+//       operacao != "/") {
+
+//     alert("Operação inválida.");
+
+//   }
+
+// } while (operacao != "+" && 
+//         operacao != "-" && 
+//         operacao != "*" && 
+//         operacao != "/");
+
+// alert("Opção válida!");
